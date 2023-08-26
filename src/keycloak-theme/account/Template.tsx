6851f9c52a6b982b5cf646fd08e0800a161086e0
Cedric Rische <cedricrische@cedsoft.de>
2023-08-26 15:20:02 +0000

    fixed storybook and started to add mui to account

--- src/keycloak-theme/account/Template.tsx
+++ src/keycloak-theme/account/Template.tsx
@@ -7,6 +7,9 @@ import { useGetClassName } from "keycloakify/account/lib/useGetClassName";
 import type { KcContext } from "./kcContext";
 import type { I18n } from "./i18n";
 import { assert } from "keycloakify/tools/assert";
+import React from "react";
+import {Button, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar} from "@mui/material";
+import {AccountCircle, Api, Description, Devices, Password, ScreenLockPortrait} from "@mui/icons-material";
 
 export default function Template(props: TemplateProps<KcContext, I18n>) {
     const { kcContext, i18n, doUseDefaultCss, active, classes, children } = props;
@@ -16,7 +19,11 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
     const { msg, changeLocale, labelBySupportedLanguageTag, currentLanguageTag } = i18n;
 
     const { locale, url, features, realm, message, referrer } = kcContext;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
     const { isReady } = usePrepareTemplate({
         "doFetchDefaultThemeResources": doUseDefaultCss,
         url,
@@ -30,13 +37,130 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
         return null;
     }
 
+    /*
+    <ul>
+                        <li className={clsx(active === "account" && "active")}>
+                            <a href={url.accountUrl}>{msg("account")}</a>
+                        </li>
+                        {features.passwordUpdateSupported && (
+                            <li className={clsx(active === "password" && "active")}>
+                                <a href={url.passwordUrl}>{msg("password")}</a>
+                            </li>
+                        )}
+                        <li className={clsx(active === "totp" && "active")}>
+                            <a href={url.totpUrl}>{msg("authenticator")}</a>
+                        </li>
+                        {features.identityFederation && (
+                            <li className={clsx(active === "social" && "active")}>
+                                <a href={url.socialUrl}>{msg("federatedIdentity")}</a>
+                            </li>
+                        )}
+                        <li className={clsx(active === "sessions" && "active")}>
+                            <a href={url.sessionsUrl}>{msg("sessions")}</a>
+                        </li>
+                        <li className={clsx(active === "applications" && "active")}>
+                            <a href={url.applicationsUrl}>{msg("applications")}</a>
+                        </li>
+                        {features.log && (
+                            <li className={clsx(active === "log" && "active")}>
+                                <a href={url.logUrl}>{msg("log")}</a>
+                            </li>
+                        )}
+                        {realm.userManagedAccessAllowed && features.authorization && (
+                            <li className={clsx(active === "authorization" && "active")}>
+                                <a href={url.resourceUrl}>{msg("myResources")}</a>
+                            </li>
+                        )}
+                    </ul>
+     */
+
+    const drawer = (
+            <div>
+                <Toolbar/>
+                <Divider/>
+                <List>
+                    <ListItemButton component={"a"} key={"account"} selected={active === "account"} disablePadding href={url.accountUrl}>
+                        <ListItemButton>
+                            <ListItemIcon>                                <AccountCircle/>
+                            </ListItemIcon>
+                            <ListItemText primary={msg("account")}/>
+                        </ListItemButton>
+                    </ListItemButton>
+                    {features.passwordUpdateSupported && (
+                        <ListItemButton key={"password"} selected={active === "password"} disablePadding href={url.passwordUrl}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Password/>
+                                </ListItemIcon>
+                                <ListItemText primary={msg("password")}/>
+                            </ListItemButton>
+                        </ListItemButton>
+                    )}
+                    <ListItemButton key={"totp"} selected={active === "totp"} disablePadding href={url.totpUrl}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ScreenLockPortrait/>
+                            </ListItemIcon>
+                            <ListItemText primary={msg("authenticator")}/>
+                        </ListItemButton>
+                    </ListItemButton>
+                    {features.identityFederation && (
+                        <ListItemButton key={"social"} selected={active === "social"} disablePadding href={url.socialUrl}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <ScreenLockPortrait/>
+                                </ListItemIcon>
+                                <ListItemText primary={msg("federatedIdentity")}/>
+                            </ListItemButton>
+                        </ListItemButton>
+                    )}
+                    <ListItemButton key={"social"} selected={active === "sessions"} disablePadding href={url.sessionsUrl}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <Devices/>
+                            </ListItemIcon>
+                            <ListItemText primary={msg("sessions")}/>
+                        </ListItemButton>
+                    </ListItemButton>
+                    <ListItemButton key={"applications"} selected={active === "applications"} disablePadding href={url.applicationsUrl}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <Api/>
+                            </ListItemIcon>
+                            <ListItemText primary={msg("applications")}/>
+                        </ListItemButton>
+                    </ListItemButton>
+                    {features.log && (
+                        <ListItemButton key={"log"} selected={active === "log"} disablePadding href={url.logUrl}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Description/>
+                                </ListItemIcon>
+                                <ListItemText primary={msg("log")}/>
+                            </ListItemButton>
+                        </ListItemButton>
+                    )}
+                    {realm.userManagedAccessAllowed && features.authorization && (
+                        <ListItemButton key={"authorization"} selected={active === "authorization"} disablePadding href={url.resourceUrl}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Description/>
+                                </ListItemIcon>
+                                <ListItemText primary={msg("myResources")}/>
+                            </ListItemButton>
+                        </ListItemButton>
+                    )}
+            </List>
+        </div>
+    );
+
     return (
         <>
             <header className="navbar navbar-default navbar-pf navbar-main header">
                 <nav className="navbar" role="navigation">
                     <div className="navbar-header">
                         <div className="container">
-                            <h1 className="navbar-title">Keycloak</h1>
+                            <h1 className="navbar-title" style={{backgroundImage: "url(https://cedsoft.de/static/media/cedsoftMainWhite.c7507176.svg)"}}>CedSoft</h1>
                         </div>
                     </div>
                     <div className="navbar-collapse navbar-collapse-1">
