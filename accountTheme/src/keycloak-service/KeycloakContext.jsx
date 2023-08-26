8a3fb2d570f0d89420310c92eaf35bc26fe8da14
Cedric Rische <cedric.rische@cedsoft.de>
2023-08-26 15:35:33 +0200

    Moved to v7 template

--- accountTheme/src/keycloak-service/KeycloakContext.jsx
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-const React = require("react");
-exports.KeycloakContext = React.createContext(undefined);
-//# sourceMappingURL=KeycloakContext.jsx.map
